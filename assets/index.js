e7e5bef54a4002b59544fc590c133e6a85546161
h3samayoa <h3samayoa@gmail.com>
2021-09-27 16:30:53 -0700

    added slider and buttons

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,11 @@ $('.settings').click(function() {
 $('.close-modal').click(function() {
     modalElements.removeClass('active');
 })
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
