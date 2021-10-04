7c1f056cfc18ebe5c20f09a4ef9af9cc8ac4192e
RoselaAntonio <roselakagamine@gmail.com>
2021-10-03 23:10:50 -0700

    <styled search bar and added more info in readme.md file>

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
-# project-1 Recipe Wizard
-Description:
-    Recipe Wizard is a webiste that allows users to receive recommendations of recipes when given their preferences. The site also allows those who want more recipe options to find a cookbook that they may want.
+# Recipe Wizard
+![Capture](https://user-images.githubusercontent.com/16126060/135796762-855760bd-b2b5-41a4-8072-52d72a542f7b.PNG)
 
-Site image:
-<img src= "./assets/images/site-image"/>
+# Description
+Recipe Wizard is a site in which the user can receive suggested recipes based on a certain number of criteria: amount of calories, time of day, type of meal, and also diet restrictions. In addition, we are able to suggest cookbooks for those who need additional recipes.
 
-Upon opening the site, the user will be able to choose their preferences and diet restrictions.
-(insert image of preferences/diet checkboxes) 
+Due to the pandemic, many of us have started to cook at home. By developing Recipe Wizard, the average person can look up recipes with ease while also including their preferences in one site. 
 
-With the information given by the user, Recipe Wizard can display 5 recipe suggestions that fit the users' criteria.
-(insert image )
+# USER STORY
+AS A homecook 
+I WANT a recipe that fulfills my nutritional needs
+SO THAT I can plan a meal efficiently
+GIVEN a webpage that meets my recipe needs
+WHEN I give specificifications about the time of day, the minimum/maximum calories, and dish type
+THEN I get recipe recommendations that fit the parameters given
 
-Deployed Site URL: 
-Github URL: https://github.com/kg-phantom/recipe-wizard
+# Directions for future deployment
+1.More preferences may be added to further allow users to modify their recipe suggestions.
+2.A bookmarking system in which users can save their favorite recipes
+3.Adding options to share recipe suggestions to others via social media accounts.
 
+# Live App Url
+Deployed link: https://kg-phantom.github.io/recipe-wizard/
+Github Link: https://github.com/kg-phantom/recipe-wizard
--- assets/index.js
+++ assets/index.js
@@ -1,74 +1,74 @@
-// query selectors for inputs
-// var timeOfDay = document.querySelector('');
-// var calorieCount = document.querySelector('');
-// var dishType = document.querySelector('');
+ // query selectors for inputs
+var timeOfDay = document.querySelector('.time-of-day');
+var calorieCount = document.querySelector('#output');
+var dishType = document.querySelector('.meal-type');
 // query selector for recipe container
-var recipeDivEl = document.querySelector('recipe-display');
-// misc variables
-var recipeDisplayEl = document.querySelector('.recipe-display');
-var recipeIdCounter = 0;
+const img = document.querySelectorAll(".recipeImg");
+const recipeLabel = document.querySelectorAll(".recipeText");
+const card = document.querySelectorAll("#card")
+let apiUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=011d75e3&app_key=a72bc3edeb9e8c56d05a5b3951a5a64f&random=true";
 
+const randomSetInt = (range, count) => {
+    let nums = new Set();
+    while (nums.size < count) {
+        nums.add(Math.floor(Math.random() * (range - 1 + 1) + 1));
+    }
+    return [...nums];
+}
 
+let randomNum = randomSetInt(19, 6)
+console.log(randomNum)
 
-// get user input and fetch recipes
-
-// gather user input parameters
-
-// Create variables to hold user input parameters
-
-// Send parameters to fetch() function to get a list of recipe datapoints
-var recipeFetch = function() {
-    var apiUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=011d75e3&app_key=a72bc3edeb9e8c56d05a5b3951a5a64f";
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        var selectionArray = [0, 1, 2, 3, 4];
-        for(var i = 0; i < selectionArray.length; i++)
-        // assign variable to the elements returned from the api call
-        var recipeName = data['hits'][selectionArray[i]]['recipe']['label'];
-        var recipeImg = data['hits'][selectionArray[i]]['recipe']['image'];
-        // not sure what other data we want to display for the recipes, but it will be stored as variables here
-
-        // package data returned as an array
-        var recipeDataObj = {
-            name: recipeName,
-            image: recipeImg
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data.hits)
+            for (var i = 0; i < 6; i++) {
+                img[i].src = data.hits[randomNum[i]].recipe.image;
+                recipeLabel[i].innerHTML = data.hits[randomNum[i]].recipe.label;
+                console.log(data.hits[randomNum[i]].recipe.shareAs);
+                let recipeApiUrl = data.hits[randomNum[i]].recipe.shareAs
+                card[i].onclick = function () {
+                    location.href = recipeApiUrl;
+                }
+                console.log(data.hits[randomNum[i]].recipe.label);
+            }
         }
-        // send recipe data to function to create recipe display
-        generateRecipes(recipeDataObj);
+    };
+    xhttp.open("GET", apiUrl, true);
+    xhttp.send();
+}
+
+loadDoc();
+
+// John's go at the dropdown menu
+$('#checkboxlist').hover(function () {
+
+   var is_open =
+   $(this).hasClass("open");
+   if(is_open) {
+       $(this).removeClass("open");
+   } 
+   else {
+       $(this).addClass("open");
+   }
+  
     });
-};
-var generateRecipes = function(recipeDataObj) {
-     // generate elements for each recipe
-     var recipeContainerEl = document.createElement('div');
-     recipeContainerEl.className = "";
-     // create id as a custom attribute
-     recipeContainerEl.setAttribute('data-recipe-id', recipeIdCounter);
-     recipeContainerEl.style.background = recipeDataObj.image;
-     // create title
-     var titleEl = document.createElement('h2');
-     titleEl.textContent = recipeDataObj.name;
-     titleEl.className = "";
-     recipeContainerEl.appendChild(titleEl);
-     
-     recipeDisplayEl.appendChild(recipeContainerEl);
+// expanded check capability
+$(document).on('click','li',function(){
+        $(this).find('input[type="checkbox"]').prop('checked',true);
+       });
 
-     // increment recipeIdCounter by one for next recipe
-     recipeIdCounter++;
-};
+function getInputVal() {
+    const inputVal = document.querySelector('input').value;
+    console.log(inputVal);
+    return inputVal;
+}
 
 
-// filter results based off of intolerances/diet checkboxes
-
-// display dropdown menu of nutritional info when a recipe is clicked
-// var dropDown = function(event) {
-//     event.preventDefault();
-    // need to create a modal here but I don't know how to yet :/
-// };
-
-// recipeDivEl.addEventListener('click', dropDown);
-
-const modalElements = $('.modal-overlay, .modal');
+const modalElements = $('#preferences-overlay, #preferences');
 
 $('.settings').click(function() {
     modalElements.addClass('active');
@@ -78,6 +78,41 @@ $('.close-modal').click(function() {
     modalElements.removeClass('active');
 })
 
+let mealType = "";
+let dishTypeModal = "";
+
+$('.submitPref').click(function() {
+    var inputVal = getInputVal();
+    apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${inputVal}&app_id=011d75e3&app_key=a72bc3edeb9e8c56d05a5b3951a5a64f&mealType=${mealType}&dishType=${dishTypeModal}&calories=${slider.value}&random=true`
+    console.log(apiUrl)
+    loadDoc();
+    modalElements.removeClass('active');
+})
+
+$('.breakfast').click(function() {
+    mealType = "Breakfast"
+})
+
+$('.lunch').click(function() {
+    mealType = "Lunch";
+})
+
+$('.dinner').click(function() {
+    mealType = "Dinner";
+})
+
+$('.starter').click(function() {
+    dishTypeModal = "Starter";
+})
+
+$('.entree').click(function() {
+    dishTypeModal = "Main%20course";
+})
+
+$('.dessert').click(function() {
+    dishTypeModal = "Sweets";
+})
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("output");
 output.innerHTML = slider.value;
@@ -85,3 +120,99 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+$("#cookbook-submit").on("click", function(event) {
+    event.preventDefault();
+    var searchTerm = $("#cookbook-search").val().trim();
+    var apiUrl = `https://openlibrary.org/search.json?q=${searchTerm}+cookbook`
+
+    fetch(apiUrl)
+    .then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                if($("#cookbooks a")) {
+                    $("#cookbooks a").each(function() {
+                        this.remove();
+                    })
+                    $("#cookbooks br").each(function() {
+                        this.remove();
+                    })
+                }
+                if($(".no-results")) {
+                    $(".no-results").remove();
+                }
+                if(data.numFound != 0) {
+                    for(var i = 0; i < 6; i++) {
+                        if(data.docs[i]) {
+                            var bookNum = randomInt(0, (data.docs.length - 1));
+                            var bookTitle = data.docs[bookNum].title;
+                            var bookSuggestEl = $("<a></a>");
+                            var bookCard = $("<div></div>").addClass("uk-card uk-card-default uk-card-hover");
+                            var bookCardBody = $("<div></div>").addClass("uk-card-body uk-card-title uk-text-center");
+                            bookCardBody.text(bookTitle);
+                            if(data.docs[bookNum].isbn) {
+                                var isbn = data.docs[bookNum].isbn[0];
+                                bookSuggestEl.attr("href", "https://openlibrary.org/isbn/" + isbn);
+                                var coverKey = "isbn";
+                                var coverKeyValue = isbn;
+                            }
+                            else if(data.docs[bookNum].oclc) {
+                                var oclc = data.docs[bookNum].oclc[0];
+                                bookSuggestEl.attr("href", "https://openlibrary.org/oclc/" + oclc);
+                                var coverKey = "oclc";
+                                var coverKeyValue = oclc;
+                            }
+                            else if(data.docs[bookNum].lccn) {
+                                var lccn = data.docs[bookNum].lccn[0];
+                                bookSuggestEl.attr("href", "https://openlibrary.org/lccn/" + lccn);
+                                var coverKey = "lccn";
+                                var coverKeyValue = lccn;
+                            }
+                            else {
+                                var olid = data.docs[bookNum].olid[0];
+                                bookSuggestEl.attr("href", "https://openlibrary.org/lccn/" + lccn);
+                                var coverKey = "olid";
+                                var coverKeyValue = olid;
+                            }
+                            var coverUrl = `http://covers.openlibrary.org/b/${coverKey}/${coverKeyValue}-M.jpg`;
+                            var coverImgDiv = $("<div></div>").addClass("uk-card-media-top uk-text-center");
+                            var coverImg = $("<img />").attr("src", coverUrl);
+                            coverImg.addClass("uk-margin-small-top");
+                            bookSuggestEl.attr("target", "_blank");
+                            coverImgDiv.append(coverImg);
+                            bookCard.append(coverImgDiv);
+                            bookCard.append(bookCardBody);
+                            bookSuggestEl.append(bookCard);
+                            $(".cookbook-suggestions").append(bookSuggestEl);
+                        }  
+                    }
+                }
+                else {
+                    var noResultsEl = $("<p></p>").text("There are no cookbooks for \"" + searchTerm + "\".");
+                    noResultsEl.addClass("no-results");
+                    $("#cookbooks").append(noResultsEl);
+                }
+            });
+        }
+        else {
+            var cookbookModal = $("#cookbook-overlay, #cookbook-modal");
+            cookbookModal.addClass("active");
+            $(".close-modal").on("click", function() {
+                cookbookModal.removeClass("active");
+            })
+            $("#ok-btn").on("click", function() {
+                cookbookModal.removeClass("active");
+            });
+        }
+    })
+    .catch(function(error) {
+        var cookbookModal = $("#cookbook-overlay, #cookbook-modal");
+        cookbookModal.addClass("active");
+        $(".close-modal").on("click", function() {
+            cookbookModal.removeClass("active");
+        })
+        $("#ok-btn").on("click", function() {
+            cookbookModal.removeClass("active");
+        })
+    })
+});
